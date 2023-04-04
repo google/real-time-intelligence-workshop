e78d9360d7e09692eff3c9e8777a67cd5d44432b
MB <mbarry323@gmail.com>
2023-04-04 15:31:15 -0400

    added license headers

--- RealTimePrediction/realtime-intelligence-main/realtime/create_sample_input.sh
+++ RealTimePrediction/realtime-intelligence-main/realtime/create_sample_input.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 bq query --nouse_legacy_sql --format=sparse \
     "SELECT EVENT_DATA FROM dsongcp.flights_simevents WHERE EVENT_TYPE = 'wheelsoff' AND EVENT_TIME BETWEEN '2015-03-10T10:00:00' AND '2015-03-10T14:00:00' " \
     | grep FL_DATE \
